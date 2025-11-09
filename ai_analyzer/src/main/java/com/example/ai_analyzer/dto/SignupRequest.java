4b94e8a44012ca832a5c04ed37405afbb222beb2
VASANTH S <134638576+vasanthvnr@users.noreply.github.com>
2025-11-09 09:47:53 +0530

    Remove phoneNumber from SignupRequest
    Removed phoneNumber field from SignupRequest class.

--- ai_analyzer/src/main/java/com/example/ai_analyzer/dto/SignupRequest.java
+++ ai_analyzer/src/main/java/com/example/ai_analyzer/dto/SignupRequest.java
@@ -6,7 +6,6 @@ import lombok.Data;
 public class SignupRequest {
     private String name;
     private String email;
-    private String phoneNumber;
     private String password;
     private String healthIssues;
 }
